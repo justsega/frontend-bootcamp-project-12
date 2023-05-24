aadfabeb1f81ef047314e9fc34516f62e95089e0
Sergey <sergsyrom@gmail.com>
2023-05-24 20:22:28 +0500

    step 8

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   BrowserRouter as Router,
   Routes,
@@ -7,67 +7,35 @@ import {
   Link,
   useLocation,
 } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Button, Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 import LoginPage from './components/LoginPage';
 import NotFoundPage from './components/NotFoundPage';
 import MainPage from './components/chat/MainPage';
-import AuthContext from './contexts';
-import useAuth from './hooks';
-
-const AuthProvider = ({ children }) => {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const logIn = () => setLoggedIn(true);
-  const logOut = () => {
-    localStorage.removeItem('userId');
-    setLoggedIn(false);
-  };
-
-  return (
-    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-const PrivateRoute = ({ children }) => {
-  const auth = useAuth();
-  const location = useLocation();
-
-  return (
-    auth.loggedIn ? children : <Navigate to="/login" state={{ from: location }} />
-  );
-};
-
-const AuthButton = () => {
-  const auth = useAuth();
-
-  return (
-    auth.loggedIn
-      ? <Button onClick={auth.logOut}>Выйти</Button>
-      : null
-  );
-};
+import SignUp from './components/SignUp';
+import { AuthProvider, AuthButton, PrivateRoute } from './providers/AuthProvider';
+import { SocketProvider } from './providers/SocketProvider';
 
 const App = () => (
   <div className='d-flex flex-column h-100'>
     <AuthProvider>
-    <Router>
-    <Navbar expand="lg" bg="white" className='shadow-sm'>
-        <Container>
-        <Navbar.Brand as={Link} to="/">Slack chat</Navbar.Brand>
-        <Nav className="mr-auto">
-        </Nav>
-        <AuthButton />
-        </Container>
-      </Navbar> 
-        <Routes>
-          <Route path="*" element={<NotFoundPage />} />
-          <Route path="/" element={(<PrivateRoute><MainPage /></PrivateRoute>)} />
-          <Route path="/login" element={<LoginPage />} />
-        </Routes>
-    </Router>
+      <SocketProvider>
+        <Router>
+          <Navbar expand="lg" bg="white" className='shadow-sm'>
+            <Container>
+              <Navbar.Brand as={Link} to="/">Hexlet chat</Navbar.Brand>
+                <Nav className="mr-auto"></Nav>
+                <AuthButton />
+            </Container>
+          </Navbar> 
+          <Routes>
+            <Route path="*" element={<NotFoundPage />} />
+            <Route path="/" element={(<PrivateRoute><MainPage /></PrivateRoute>)} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+      </Router>
+    </SocketProvider>
     </AuthProvider>
   </div>
 );
